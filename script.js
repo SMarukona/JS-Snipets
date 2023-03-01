afa7e969d01da8573909cf560ee6c6d85c8048e1
Sanjeewa Marukona <smarukonaa@gmail.com>
2023-03-01 20:52:40 +0530

    Class inheritance

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //Defining Javascript Class
+class Person {
+    constructor(name, age){
+       this.name= name;
+       this.age = age;
+    }
+    greeting(){
+        console.log(`Hello ${this.name}! You are ${this.age} years old.`);
+    }
+}
+
+
+
+
+
+//Let's extends Person class to create Politician
+
+class Politician extends Person{
+    constructor(name, age, party){
+        super(name, age);
+        this.party = party;
+    }
+
+    campaign(){
+        console.log(`${this.name} is contest for ${this.party}`);
+    }
+}
+
+//Creating instance of Politician
+
+const politician1 = new Politician("Joe", 35, "Democratic Party.");
+
+politician1.greeting();
+politician1.campaign();
