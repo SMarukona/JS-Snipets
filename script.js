adc05d9860b127c0c5740aa07999aa7e309e255a
Sanjeewa Marukona <smarukonaa@gmail.com>
2023-03-02 17:35:50 +0530

    This keyword practice

--- script.js
+++ script.js
@@ -1,34 +1,77 @@
-//Defining Javascript Class
-class Person {
+// //Defining Javascript Class
+// class Person {
+//     constructor(name, age){
+//        this.name= name;
+//        this.age = age;
+//     }
+//     greeting(){
+//         console.log(`Hello ${this.name}! You are ${this.age} years old.`);
+//     }
+// }
+
+// //Let's extends Person class to create Politician
+
+// class Politician extends Person{
+//     constructor(name, age, party){
+//         super(name, age);
+//         this.party = party;
+//     }
+
+//     campaign(){
+//         console.log(`${this.name} is contest for ${this.party}`);
+//     }
+// }
+
+// //Creating instance of Politician
+
+// const politician1 = new Politician("Joe", 35, "Democratic Party.");
+
+// politician1.greeting();
+// politician1.campaign();
+
+//Javascript this keyword
+//This keyword in the function
+function helloWorld(){
+    console.log("Hellow world");
+    console.log(this);
+}
+helloWorld();
+
+//This keyword in JS object
+const Bird = {
+    name: "Sparrow",
+    beakType: "Short",
+    eat: function(){
+        console.log(this.name + " eats rice");
+    },
+    telleaktype: function(){
+        console.log(this.beakType);
+    }
+
+}
+
+Bird.eat();
+Bird.telleaktype();
+
+//Accessing this while maintaining context
+function telleaktype1(){
+    console.log(this.beakType);
+}
+Bird.beak = telleaktype1;
+Bird.beak();
+telleaktype1();
+
+//This on constructor function
+class Person{
     constructor(name, age){
-       this.name= name;
-       this.age = age;
+       this.name = name,
+       this.age = age
     }
-    greeting(){
-        console.log(`Hello ${this.name}! You are ${this.age} years old.`);
-    }
-}
-
-
-
-
-
-//Let's extends Person class to create Politician
-
-class Politician extends Person{
-    constructor(name, age, party){
-        super(name, age);
-        this.party = party;
-    }
-
-    campaign(){
-        console.log(`${this.name} is contest for ${this.party}`);
+    intro(){
+        console.log("My name is " + this.name + " and I'm " + this.age + " old.");
     }
 }
 
-//Creating instance of Politician
+const person1 = new Person("Mike", 25);
 
-const politician1 = new Politician("Joe", 35, "Democratic Party.");
-
-politician1.greeting();
-politician1.campaign();
+person1.intro();
